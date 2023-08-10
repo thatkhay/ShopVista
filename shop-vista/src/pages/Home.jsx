8c52b7a0d9c4ae6db1d889b17d712d10ea4e9bdb
thatkhay <johnkingsleyegeonu@gmail.com>
2023-08-10 21:32:16 +0100

    responsiveness

--- shop-vista/src/pages/Home.jsx
+++ shop-vista/src/pages/Home.jsx
@@ -36,10 +36,10 @@ function Home() {
   return (
    
     
-    <Container>
+    <Container style={{padding: isMobile ? '0' : 'auto'}}>
    <Header/>
    <Modal products={products}/>
-   <Container style={{alignItems: 'center', display: 'flex', marginTop: '3rem', flexDirection: isMobile ? 'column' : 'row', gap: tabSize ? '3rem' : ''}}>
+   <Container style={{alignItems: 'center', display: 'flex', marginTop: '3rem', flexDirection: isMobile ? 'column' : 'row', gap: tabSize ? '3rem' : '1rem'}}>
    <main style={{ display: 'flex' , alignItems: 'center' ,  flexDirection: 'column' , width: '50%'}}>
     <div style={{position: 'relative', zIndex: 50}}>
     <img src={mainImg} alt="" style={{ height: '20rem', width: '18rem', borderRadius: '.7rem' }} />
@@ -57,17 +57,18 @@ function Home() {
     </main>
   
 
-    <main style={{width: '50%'}}>
-   <h2>Sneaker Company</h2>
-   <h1>Fall limited sneaker</h1>
-   <p>these low-profile sneaker are your perfect causual wear companion. featuring a durable rubber outer sole, they'll withstand everything the weather can offer</p>
-   <div>
+    <main style={{width: isMobile ? '80%' : '50%'}}>
+   <h2 style={{textTransform: 'uppercase', color: 'hsl(26, 100%, 55%)', fontSize: '.6rem', fontWeight: 900, letterSpacing: '.8px'}}>Sneaker Company</h2>
+   <h1 style={{width: isMobile? '100%' : '70%', fontWeight: 700, textTransform: 'capitalize'}}>Fall limited edition sneaker</h1>
+   <p style={{fontSize: '.8rem', color: ' hsl(219, 9%, 45%)', fontWeight: 700, textTransform: 'capitalize'}}>these low-profile sneaker are your perfect causual wear companion. featuring a durable rubber outer sole, they'll withstand everything the weather can offer</p>
 
-    <div >
-      <span>$125.00</span>
-      <span>50%</span>
+   <div style={{display: 'flex', justifyContent: isMobile ? 'space-between' : '0', width: isMobile ? '100%' : '13rem', flexDirection: isMobile ? 'column' : 'row'}}>
+
+    <div style={{display: 'flex', alignItems:'center' , gap: '1rem', marginTop: '1.5rem'}} >
+      <span style={{fontSize: '1.2rem', color: 'black', fontWeight: 900}}>$125.00</span>
+      <span style={{fontSize: '.7rem', backgroundColor: 'hsl(25, 100%, 94%)', padding: '.2rem .3rem', borderRadius: '.3rem'}}>50%</span>
     </div>
-    <span style={{textDecoration: 'line-through'}}>$250</span>
+    <span style={{textDecoration: 'line-through', fontSize: '.7rem', marginTop: isMobile ? '1rem' : '2rem', color: 'hsl(220, 14%, 75%)'}}>$250</span>
    </div>
 
    <div>
