d7c7a5a2be47978dd5871cd5a89625c5251480cc
thatkhay <johnkingsleyegeonu@gmail.com>
2023-08-10 14:25:16 +0100

    .

--- shop-vista/src/components/Modal.jsx
+++ shop-vista/src/components/Modal.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 
-const Modal = () => {
+const Modal = ({ products }) => {
   return (
-    <div>Modal</div>
-  )
+    // <div style={{ backgroundColor: 'black' , position: 'absolute', top: 0, left: 0, bottom: 0, right: 0, opacity: 0.5}}>
+      // <h1 style={{ color: 'red', fontSize: '6rem'}}>Modal</h1>
+  //   </div>
+  <></>
+   )
 }
 
 export default Modal
--- shop-vista/src/pages/Home.jsx
+++ shop-vista/src/pages/Home.jsx
@@ -2,30 +2,53 @@ import React, { useState } from 'react'
 import {data} from '../data'
 import Header from '../components/Header'
 import { Container } from '@mui/material'
-
+import Modal from '../components/Modal'
+import { FaChevronLeft, FaChevronRight } from 'react-icons/fa'
 function Home() {
   const [products] = useState(data)
   const [value, setValue] = useState(0)
+  const [slideIndex, setSlideIndex] = useState(1)
 
   const { mainImg } = products[value]
 
+ const nextSlide = () => {
+  if (slideIndex !== products.length){
+    setSlideIndex(slideIndex + 1)
+  }else if  (slideIndex === products.length){
+    setSlideIndex(1)
+  }
+ }
+ const prevSlide = () => {
+  if (slideIndex !== products.length){
+    setSlideIndex(slideIndex - 1)
+  }else if  (slideIndex === products.length){
+    setSlideIndex(1)
+  }
+ }
+
   return (
+   
     
     <Container>
    <Header/>
+   <Modal products={products}/>
    <Container style={{alignItems: 'center', display: 'flex', marginTop: '3rem', justifyContent: 'space-between'}}>
    <main style={{ display: 'flex' , alignItems: 'center' ,  flexDirection: 'column' , width: '50%'}}>
-    <div >
+    <div style={{position: 'relative', zIndex: 50}}>
     <img src={mainImg} alt="" style={{ height: '20rem', width: '18rem', borderRadius: '.7rem' }} />
+    <div>
+      <button style={{ backgroundColor: 'white', borderRadius: '50%', padding: '1rem', boxShadow: 'box-shadow: -3px -3px 6px rgba(0, 0, 0, 0.4)', position: 'absolute', right: '1rem', top: '7rem' }}><FaChevronRight/></button>
+      <button style={{ backgroundColor: 'white', borderRadius: '50%', padding: '1rem', boxShadow: 'box-shadow: -3px -3px 6px rgba(0, 0, 0, 0.4)', position: 'absolute', right: '14rem', top: '7rem' }}><FaChevronLeft/></button>
+    </div>
     </div>
 
     <ul style={{display: 'flex', alignItems: 'center', justifyContent: 'space-between', width: '18rem', marginLeft: '-2rem'}}>
     {products.map((item, index) => (
-    <li style={{ display: 'flex' , height: '3.5rem', width: '3.5rem', border: index === value && '2px solid hsl(26, 100%, 55%)', borderRadius: '.6rem', backgroundColor: index === value && 'hsl(25, 100%, 94%)', overflow: index === value && 'hidden'}} key={item.id} onClick={() => setValue(index)}> <img src={item.thumbnail} alt=""  style={{borderRadius: '.4rem'}}/> </li>
+    <li style={{ display: 'flex' , height: '3.5rem', width: '3.5rem', border: index === value && '2px solid hsl(26, 100%, 55%)', borderRadius: '.6rem', opacity: index === value && 0.5 , overflow: index === value && 'hidden'}} key={item.id} onClick={() => setValue(index)}> <img src={item.thumbnail} alt=""  style={{borderRadius: '.4rem'}}/> </li>
      ))}
     </ul>
     </main>
-
+  
 
     <main style={{width: '50%'}}>
    <h2>Sneaker Company</h2>
